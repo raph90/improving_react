abddb6be2c0fa9251858a30948d1b53f62bb7c42
Raphael Hetherington <raph37heth@gmail.com>
2021-04-09 15:47:33 +0100

    basic state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Hello!</div>;
+  const [on, setOn] = React.useState(false);
+  return (
+    <div className="App">
+      Hello {on.toString()}
+      <button onClick={() => setOn((old) => !old)}>Toggle</button>
+    </div>
+  );
 }
 
 export default App;
